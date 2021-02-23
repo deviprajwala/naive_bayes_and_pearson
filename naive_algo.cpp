b62aacc420917f8fe7d4b2e1c15d85e66da77591
deviprajwala <deviprajwala3@gmail.com>
2021-02-23 22:56:30 +0530

    function to calculate the value of mean is included

--- naive_algo.cpp
+++ naive_algo.cpp
@@ -5,7 +5,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+float time_spent_mean=0, age_mean = 0, income_mean = 0, internet_usage_mean = 0;
+// Read the Data from the file 
+	// as String Vector 
+	vector <string> row; 
 void read_record(string data_name) 
 { 
 
@@ -16,15 +19,13 @@ void read_record(string data_name)
 	fin.open(data_name, ios::in); 
  
 
-	// Read the Data from the file 
-	// as String Vector 
-	vector <string> row; 
+	
 	string line, word, temp; 
 
 	while (fin >> temp) 
     { 
 
-		row.clear(); 
+		//row.clear(); 
 
 		// read an entire row and 
 		// store it in a string variable 'line' 
@@ -42,16 +43,31 @@ void read_record(string data_name)
 			// of a row to a vector 
 			row.push_back(word); 
 		} 
-
-		cout << row[0];
     }
 } 
 
+void calculation()
+{
+    for(int i=0;i<row.size();i++)
+    {
+        time_spent_mean += stof(row[i]);
+        age_mean += stof(row[i+1]);
+        income_mean += stof(row[i+2]);
+        internet_usage_mean += stof(row[i+3]);
+        i+=4;
+    }
+    
+    time_spent_mean /= sqrt(row.size());
+    age_mean /= sqrt(row.size());
+    income_mean /= sqrt(row.size());
+    internet_usage_mean /= sqrt(row.size());
+
+}
 int main()
 {
-    string s;// = "data.csv";
-    cin>>s;
+    string s = "data.csv";
+    //cin>>s;
     read_record(s);
-
+    calculation();
     return 0;
 }
