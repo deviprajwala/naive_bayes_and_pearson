4fc1f343a19ab9a70ec76fc47ae935faa81e5a76
deviprajwala <deviprajwala3@gmail.com>
2021-02-26 19:48:10 +0530

    window asks whether which data to be used and asks to enter the data

--- run.sh
+++ run.sh
@@ -2,34 +2,38 @@
 
 echo -e "IMPLEMENTATION OF NAIVE BAYES ALGORITHM\n\n"
 
-function enter_manually
-{
-  g++ naive.cpp -o naive;
-  ./naive;
-  echo "The entered data is"
-  cat  data.csv
-}
 
-function use_existing
+function ask_data
 {
-  echo "The existing data will be used"
+   echo -e "Daily time spent"
+   read time_spent
+   echo -e $time_spent >> user_input.txt
+   echo -e "Age"
+   read age
+   echo -e $age >>user_input.txt
+   echo -e "Income"
+   read income
+   echo -e $income >> user_input.txt
+   echo -e "Internet Usage"
+   read internet
+   echo -e $internet >> user_input.txt
+   
 }
+dialog --yesno "Do you want to enter your own data?" 10 20
+if [ $? -eq 0 ]
+then 
+    g++ naive.cpp -o naive;
+    ./naive;
+    echo "The entered data is"
+    echo "new_data.csv" > user_input.txt
+    g++ naive_algo.cpp -o algo
+    ask_data;
+    ./algo < user_input.txt
+else
+    echo "The existing data will be used"
+    g++ naive_algo.cpp -o algo
+    echo "data.csv" > user_input.txt
+    ask_data;
+    ./algo < user_input.txt
+fi
 
-function menu
-{
-  echo -e "\t Enter option regarding the choice of data\n"
-  echo -e "\t 1 to Enter data manually\n"
-  echo -e "\t 2 to Use existing data\n"
-
-  read -n 1 option
-}
- 
-menu
- 
-case $option in
-0) break;;
-1) enter_manually;;
-2) use_existing;;
-*) clear
-   echo "wrong selection";;
-esac
