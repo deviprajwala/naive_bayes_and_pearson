0b2948e1b603da37721d8aa4aaaff1878235465a
deviprajwala <deviprajwala3@gmail.com>
2021-02-26 20:17:09 +0530

    works as expected!!

--- run.sh
+++ run.sh
@@ -1,24 +1,20 @@
 
-
+#script to run the algorithm by taking various input by making use of windows 
 echo -e "IMPLEMENTATION OF NAIVE BAYES ALGORITHM\n\n"
 
 
 function ask_data
 {
-   dialog --inputbox "Enter daily time spent" 10 20 2>user_input.txt
-   echo -e "\n" 2>> user_input.txt
-   
-   echo -e "Age"
-   read age
-   echo -e $age >>user_input.txt
-   echo -e "Income"
-   read income
-   echo -e $income >> user_input.txt
-   echo -e "Internet Usage"
-   read internet
-   echo -e $internet >> user_input.txt
-   
+    dialog --backtitle "Dialog Form" --title "user input" \
+    --form "\nEnter the details" 25 60 16 \
+   "Daily time limit:" 1 1 "Value 1" 1 25 25 30 \
+   "Age:" 2 1 "Value 2" 2 25 25 30 \
+   "Income:" 3 1 "Value 3" 3 25 25 30 \
+   "Internet usage:" 4 1 "Value 4" 4 25 25 30 \
+2>user_input.txt   
 }
+#function which asks the user to enter the input into the window
+
 dialog --yesno "Do you want to enter your own data?" 10 20
 if [ $? -eq 0 ]
 then 
@@ -36,4 +32,5 @@ else
     ask_data;
     ./algo < user_input.txt
 fi
+#the above lines of code deal with 2 situations one when the user wants to enter his own data and the other when he makes use of the existing data
 
